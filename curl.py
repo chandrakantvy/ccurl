187a1f3582ddf906c89d8cc090fa2ae3c39e4033
Chandrakant Vyavahare <chandrakant@Chandrakants-MacBook-Air.local>
2024-01-14 00:05:37 +0530

    Add response print

--- curl.py
+++ curl.py
@@ -1,6 +1,7 @@
 import argparse
 from urllib.parse import urlparse
 from actions.actions import ValidateUrl
+import requests
 
 
 parser = argparse.ArgumentParser(prog="RESTful APIs test utility")
@@ -11,5 +12,19 @@ curl_parser.add_argument('url', nargs=1, action=ValidateUrl, help='request URL',
 args = parser.parse_args()
 
 
+def request_message(parsed_request_url, method='GET'):
+    print(fr'connecting to {parsed_request_url.netloc}' '\n'
+          fr'Sending request {method} /{method.lower()} {parsed_request_url.scheme.upper()}' '\n'
+          fr'Host: {parsed_request_url.hostname}' '\n'
+          fr'Accept: */*' '\n')
+
+
+def print_response(response_):
+    print(response_.json())
+
+
 if __name__ == '__main__':
-    pass
+    parsed_request_url = urlparse(args.url)
+    request_message(parsed_request_url)
+    response = requests.get(args.url)
+    print_response(response)
